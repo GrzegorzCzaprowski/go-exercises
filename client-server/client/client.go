d5ae6d1f37b85999301aefdc508d1c46c1e185b3
Grzegorz Czaprowski <gc@ges.sh>
2019-05-29 11:05:51 +0200

    poprawienie budowania urla

---
+++ client-server/client/client.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type User struct {
+	ID      uint64
+	Name    string
+	Surname string
+	Email   string
+}
+
+func postUser(user User, address string) {
+	var url string
+	url = fmt.Sprintf("%s/users/", address)
+
+	jsonValue, _ := json.Marshal(user) //zamiana danych na forma jsona
+	//zbuforowanie jasona do bytów
+	response, err := http.Post(url, "aplication/json", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(string(body))
+}
+
+func deleteUser(id uint64, address string) {
+	url := fmt.Sprintf("%s/users/%d/", address, id)
+
+	client := &http.Client{}
+
+	req, _ := http.NewRequest("DELETE", url, nil)
+
+	response, _ := client.Do(req)
+	defer response.Body.Close()
+}
+
+func getUser(id uint64, address string) {
+	url := fmt.Sprintf("%s/users/%d/", address, id)
+
+	response, _ := http.Get(url)
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(string(body))
+}
+
+func main() {
+	var flagServerAddress string
+	flag.StringVar(&flagServerAddress, "addr", "http://localhost:8000", "server address")
+	flag.Parse()
+
+	for {
+		println("choose action")
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimRight(input, "\r\n")
+		command := strings.Split(input, " ")
+		if command[0] == "adduser" {
+			var user User
+
+			print("enter ID (uint64): ")
+			input, _ := reader.ReadString('\n')
+			input = strings.TrimRight(input, "\r\n")
+			i, _ := strconv.ParseUint(input, 0, 64)
+			user.ID = i
+			println(user.ID)
+
+			print("enter Name (string): ")
+			input, _ = reader.ReadString('\n')
+			input = strings.TrimRight(input, "\r\n")
+			user.Name = input
+			println(user.Name)
+
+			print("enter Surname (string): ")
+			input, _ = reader.ReadString('\n')
+			input = strings.TrimRight(input, "\r\n")
+			user.Surname = input
+			println(user.Surname)
+
+			print("enter Email (string): ")
+			input, _ = reader.ReadString('\n')
+			input = strings.TrimRight(input, "\r\n")
+			user.Email = input
+			println(user.Email)
+
+			postUser(user, flagServerAddress)
+		} else if command[0] == "delete" {
+			userID, _ := strconv.ParseUint(command[1], 10, 64)
+			deleteUser(userID, flagServerAddress)
+		} else if command[0] == "getuser" {
+			userID, _ := strconv.ParseUint(command[1], 10, 64)
+			getUser(userID, flagServerAddress)
+		} else {
+			println("bad command")
+		}
+	}
+}
