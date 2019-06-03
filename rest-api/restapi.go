ca89e3d777734dd59c8554a92bcea337fe39b1a0
Grzegorz Czaprowski <gc@ges.sh>
2019-06-03 14:16:13 +0200

    add GETALL query

--- rest-api/restapi.go
+++ rest-api/restapi.go
@@ -17,15 +17,15 @@ type Todo struct {
 	Id          int    `json:"id,omitempty`
 	Name        string `json:"name,omitempty"`
 	Description string `json:"description,omitempty"`
-	// created_at string	`json:"CreatedAt,omitempty"`
-	// updated_at string	`json:"UpdatedAt,omitempty"`
+	Created_at  string `json:"created_at,omitempty"`
+	Updated_at  string `json:"updated_at,omitempty"`
 }
 
 func (server *Server) CreateTodo(w http.ResponseWriter, req *http.Request) {
 	todo := &Todo{}
 	err := json.NewDecoder(req.Body).Decode(&todo)
 	if err != nil {
-		log.Println("blad w decodowaniu z jsona")
+		log.Println("blad w decodowaniu z jsona", err)
 	}
 	defer req.Body.Close()
 
@@ -39,6 +39,21 @@ func (server *Server) CreateTodo(w http.ResponseWriter, req *http.Request) {
 	todo = &Todo{Id: id}
 
 	log.Println("POST poszedl")
+}
+
+func (server *Server) ReadAllTodos(w http.ResponseWriter, req *http.Request) {
+	rows, _ := server.db.Query("SELECT id, name, description, created_at, updated_at FROM todos")
+	var todos []Todo
+	for rows.Next() {
+		todo := Todo{}
+		err := rows.Scan(&todo.Id, &todo.Name, &todo.Description, &todo.Created_at, &todo.Updated_at)
+		if err != nil {
+			log.Println("nie tak sie skanuje", err)
+		}
+		todos = append(todos, todo)
+	}
+	json.NewEncoder(w).Encode(todos)
+
 }
 
 type Server struct {
@@ -58,7 +73,7 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api/todos/", server.CreateTodo).Methods("POST")
-	// router.HandleFunc("/api/todos/", ReadAllTodos).Methods("GET")
+	router.HandleFunc("/api/todos/", server.ReadAllTodos).Methods("GET")
 	// router.HandleFunc("/api/todos/:id/", ReadByID).Methods("GET")
 	// router.HandleFunc("/api/todos/:id/", UpdateTodo).Methods("PATCH")
 	// router.HandleFunc("/api/todos/:id/", RemoveTodo).Methods("DELETE")
