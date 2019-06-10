52129f80552ee7161ec265a90f2e796b1cf29c77
Grzegorz Czaprowski <gc@ges.sh>
2019-06-10 15:21:38 +0200

    rozwiazanie w 100% zgodne z poleceniem

---
+++ pingpong/pingpong2/pingpong2.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func ping(channel chan string) {
+	i := <-channel
+	fmt.Println(i)
+	channel <- "ping"
+	wg.Done()
+}
+
+func pong(channel chan string) {
+	channel <- "pong"
+	time.Sleep(time.Millisecond * time.Duration(100+rand.Intn(900)))
+	fmt.Println(<-channel)
+	wg.Done()
+}
+
+var wg = sync.WaitGroup{}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	channel := make(chan string)
+	for {
+		time.Sleep(time.Millisecond * time.Duration(100+rand.Intn(900)))
+		wg.Add(2)
+		go ping(channel)
+		go pong(channel)
+		wg.Wait()
+	}
+}
